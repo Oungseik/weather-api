a297d56de06963894a2ab52c0554e026f2829022
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-07-22 07:38:35 +0630

    feat: serve static file from hono

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
+import { serveStatic } from "@hono/node-server/serve-static";
 import dotenv from "dotenv";
 
 import { config } from "./config";
@@ -12,6 +13,7 @@ dotenv.config();
 export let app = new Hono();
 
 app.use(cors({ origin: ["127.0.0.1:*", "localhost:*"] }));
+app.use("/static/*", serveStatic({ root: "./" }));
 app.use(logger());
 
 app.route("/", checkHealthRouter);
